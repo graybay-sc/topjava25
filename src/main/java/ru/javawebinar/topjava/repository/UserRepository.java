555d0b9fc6773084a1c07e80c153c59af316b5b2
graybay <graybay@list.ru>
2022-03-09 21:46:48 +0400

    HW05 OPT 7 fix to throw UnsupportedOperationException

--- src/main/java/ru/javawebinar/topjava/repository/UserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/UserRepository.java
@@ -15,7 +15,7 @@ public interface UserRepository {
     User get(int id);
 
     default User getWithMeals(int id) {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     // null if not found
